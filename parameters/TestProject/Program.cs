69dc2850ddeba7d7b8b446fc1d266911c52ccb7a
slothgina <gina.slothstudio@zohomail.com>
2026-05-03 14:36:03 -0400

    Test with strings

--- parameters/TestProject/Program.cs
+++ parameters/TestProject/Program.cs
@@ -48,3 +48,15 @@
     //status = (isHealthy ? "Healthy" : "Unhealthy");
   //  Console.WriteLine($"Middle: {status}");
 //}
+
+string status = "Healthy";
+
+Console.WriteLine($"Start: {status}");
+SetHealth(false);
+Console.WriteLine($"End: {status}");
+
+void SetHealth(bool isHealthy) 
+{
+    status = (isHealthy ? "Healthy" : "Unhealthy");
+    Console.WriteLine($"Middle: {status}");
+}
