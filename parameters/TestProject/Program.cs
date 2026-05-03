cfa2e3fb201fb7bf5a2be02cdaf6015e97292880
slothgina <gina.slothstudio@zohomail.com>
2026-05-03 14:28:57 -0400

    Test pass by value

---
+++ parameters/TestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 3;
+int b = 4;
+int c = 0;
+
+Multiply(a, b, c);
+Console.WriteLine($"global statement: {a} x {b} = {c}");
+
+void Multiply(int a, int b, int c) 
+{
+    c = a * b;
+    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+}
