4cc1a719a780dfcde967a9fa1aae12ef894507f1
slothgina <gina.slothstudio@zohomail.com>
2026-05-03 14:32:30 -0400

    Test pass by reference

--- parameters/TestProject/Program.cs
+++ parameters/TestProject/Program.cs
@@ -1,12 +1,36 @@
-﻿int a = 3;
-int b = 4;
-int c = 0;
+﻿//int a = 3;
+//int b = 4;
+//int c = 0;
 
-Multiply(a, b, c);
-Console.WriteLine($"global statement: {a} x {b} = {c}");
+//Multiply(a, b, c);
+//Console.WriteLine($"global statement: {a} x {b} = {c}");
 
-void Multiply(int a, int b, int c) 
+//void Multiply(int a, int b, int c) 
+//{
+  //  c = a * b;
+   // Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+//}
+
+
+int[] array = {1, 2, 3, 4, 5};
+
+PrintArray(array);
+Clear(array);
+PrintArray(array);
+
+void PrintArray(int[] array) 
 {
-    c = a * b;
-    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+    foreach (int a in array) 
+    {
+        Console.Write($"{a} ");
+    }
+    Console.WriteLine();
+}
+
+void Clear(int[] array) 
+{
+    for (int i = 0; i < array.Length; i++) 
+    {
+        array[i] = 0;
+    }
 }
--- parameters/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ parameters/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa2e3fb201fb7bf5a2be02cdaf6015e97292880")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
